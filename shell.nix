434644a5087acfe136935dc2eaac931b35ff1c2d
khaser <a-horohorin@mail.ru>
2023-05-12 21:00:26 +0300

    last commit before refactoring

--- shell.nix
+++ shell.nix
@@ -6,11 +6,8 @@ pkgs.mkShell {
   name = "RISC-V hardware develop";
 
   nativeBuildInputs = with pkgs; [
-    # pkgs.pkgsCross.riscv32-embedded.gcc
     sw-toolchain
     spike
-    dtc
-    jdk # for rics-V torture
     verilator
     python310
     (callPackage /etc/nixos/vim.nix { 
